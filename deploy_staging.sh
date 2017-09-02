74dbd6217f07273c7174ef41fc729dfa034aa36b
Abhi Yerra <abhi@berkeley.edu>
2017-09-02 02:13:04 +0000

    use api-staging

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-(export API_HOST=https://staging.envkey.com/api/v1 && ./build.sh)
+(export API_HOST=https://api-staging.envkey.com/api/v1 && ./build.sh)
 sleep 1
 netlify deploy -s envkey-staging -p envkey-assets/build
