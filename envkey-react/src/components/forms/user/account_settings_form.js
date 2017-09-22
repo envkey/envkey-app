6b9d9e179b618b4c2d3204ca08e5541aea6d3487
Dane Schneider <dane.schneider@gmail.com>
2017-09-21 23:59:29 -0400

    don't allow editing first name / last name through account settings

--- envkey-react/src/components/forms/user/account_settings_form.js
+++ envkey-react/src/components/forms/user/account_settings_form.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import h from "lib/ui/hyperscript_with_helpers"
 import SmallLoader from 'components/shared/small_loader'
-import UserForm from './user_form'
+import UserDetails from './user_details'
 import DeleteField from '../shared/delete_field'
 
 export default function({
   currentUser,
   isRemoving,
-  isUpdatingSettings,
-  onRemove,
-  onUpdateSettings
+  onRemove
 }){
 
-  return h.div(".user-settings.my-account-settings", [
-    h(UserForm, {
-      user: currentUser,
-      orgRolesAssignable: [],
-      isSubmitting: isUpdatingSettings,
-      onSubmit: onUpdateSettings,
-      isAccountForm: true
-    }),
+  return h.div(".settings.user-settings.my-account-settings", [
+    h(UserDetails, currentUser),
 
     h.div(".danger-zone", [
       h.h3("Danger Zone"),
-      h(DeleteField, {
-        label: "Your Account",
-        isRemoving,
-        onRemove,
-        confirmName: [currentUser.firstName, currentUser.lastName].join(" "),
-        confirmPrompt: "your full name"
-      })
+      h.div(".content", [
+        h.fieldset(".delete-account", [
+          h.label("Delete Account"),
+          h(DeleteField, {
+            label: "Your Account",
+            isRemoving,
+            onRemove,
+            confirmName: [currentUser.firstName, currentUser.lastName].join(" "),
+            confirmPrompt: "your full name"
+          })
+        ])
+      ])
     ])
   ])
 }
