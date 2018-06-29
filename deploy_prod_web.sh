3bdbabfb871a420b57b2ba1e1643c0f062a69b8a
Dane Schneider <dane.schneider@gmail.com>
2018-06-29 16:31:55 -0700

    web testing deploy script updates

--- deploy_prod_web.sh
+++ deploy_prod_web.sh
@@ -2,4 +2,6 @@
 
 (K8S_BUILD=true && ./build_prod.sh)
 sleep 1
+rm netlify.toml
 netlifyctl deploy -n envkey-k8s -P envkey-assets/build
+rm netlify.toml
--- deploy_staging_web.sh
+++ deploy_staging_web.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-(export API_HOST=https://api-staging.envkey.com/api/v1 && ./build.sh)
+(export API_HOST=https://api-staging.envkey.com/api && ./build.sh)
 sleep 1
+rm netlify.toml
 netlifyctl deploy -n envkey-staging -P envkey-assets/build
+rm netlify.toml
