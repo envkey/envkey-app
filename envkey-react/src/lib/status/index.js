45b892420d5156e9401c78ad2ac740843efb72d3
Dane Schneider <dane.schneider@gmail.com>
2021-07-30 16:36:38 -0700

    Make internet connectivity test more robust to resolve rare firewall issues

--- envkey-react/src/lib/status/index.js
+++ envkey-react/src/lib/status/index.js
@@ -2,6 +2,7 @@ import {store} from 'init_redux'
 import {getIsUpdatingAnyEnv} from 'selectors'
 import {DISCONNECTED, REACTIVATED_BRIEF, REACTIVATED_LONG, commitSanitizedActionLog} from 'actions'
 import isElectron from 'is-electron'
+import R from 'ramda'
 
 window.isUpdatingAnyEnv = ()=> {
   return getIsUpdatingAnyEnv(store.getState())
@@ -15,31 +16,42 @@ let isCheckingConnection = false,
 
 const
   isOnline = (retries)=>{
-    if (process.env.NODE_ENV == 'development'){
-      return Promise.resolve(true)
-    }
+    // if (process.env.NODE_ENV == 'development'){
+    //   return Promise.resolve(true)
+    // }
 
-    const url = isElectron() ?
-      "http://www.msftconnecttest.com/connecttest.txt" :
-      "https://icanhazip.com/"
+    const urls = isElectron() ?
+      ["http://www.msftconnecttest.com/connecttest.txt", "https://status.aws.amazon.com"] :
+      ["https://icanhazip.com/"]
 
-    return fetch((url + "?" + Date.now().toString()), {
-      mode: "no-cors",
-      cache: 'no-store'
-    }).then(response => {
-      return true
-    }).catch(error => {
-      if (!retries || retries < 2){
-        return isOnline((retries || 0) + 1)
+    return Promise.all(
+      urls.map(
+        url => fetch((url + "?" + Date.now().toString()), {
+          mode: "no-cors",
+          cache: 'no-store',
+          method: 'HEAD'
+        }).then(response => {
+          return true;
+        }).catch(error => {
+          return false;
+        })
+      )
+    ).then(res => {
+      if (R.any(Boolean, res)){
+        return true;
+      } else {
+        if (!retries || retries < 2){
+          return isOnline((retries || 0) + 1);
+        }
+        return false;
       }
-      return false
     })
   },
 
   checkConnection = ()=> {
-    if (process.env.NODE_ENV == 'development'){
-      return
-    }
+    // if (process.env.NODE_ENV == 'development'){
+    //   return
+    // }
 
     isCheckingConnection = true
     const disconnected = store.getState().disconnected == true
